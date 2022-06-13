a7d123d44452b64c61ad957e3ab8db34c0c0fb04
Mrinmoy Singha <mrinmoysingha064@gmail.com>
2022-06-13 19:31:35 +0530

    gif commit

--- domain-starter/src/App.js
+++ domain-starter/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 
@@ -6,17 +6,42 @@ const TWITTER_HANDLE = "MrinmoyEth";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-	const checkIfWalletIsConnected = () => {
-		const {ethereum} = window;
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window;
 
-		if(!ethereum) {
-			console.log("Make sure you have MetaMask!");
-		}
-		else{
-			console.log("We have the ethereum object", ethereum);
-		}
-	}
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  };
 
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      <iframe
+        title="Minato"
+        src="https://giphy.com/embed/A8UFISckEbokw"
+        width="480"
+        height="258"
+        frameBorder="0"
+        class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>
+        <a href="https://giphy.com/gifs/naruto-battle-A8UFISckEbokw">
+          <br/>
+        </a>
+      </p>
+
+      <button className="cta-button connect-wallet-button">
+        Connect Wallet
+      </button>
+    </div>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  });
 
   return (
     <div className="App">
@@ -24,11 +49,13 @@ const App = () => {
         <div className="header-container">
           <header>
             <div className="left">
-              <p className="title">🤞 Yolo Name Service</p>
+              <p className="title">🤞 Kamui Name Service</p>
               <p className="subtitle">Your eternal API on the blockchain!</p>
             </div>
           </header>
         </div>
+
+        {renderNotConnectedContainer()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
